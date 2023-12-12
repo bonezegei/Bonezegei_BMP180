a049539b2aef07eb0ecd44d4dd5059f978a3ab30
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-12-13 06:35:31 +0800

    Update Bonezegei_BMP180.h

--- src/Bonezegei_BMP180.h
+++ src/Bonezegei_BMP180.h
@@ -3,9 +3,14 @@
   Author: Bonezegei (Jofel Batutay)
 */
 
+#ifndef _BONEZEGEI_BMP180_H_
+#define _BONEZEGEI_BMP180_H_
+
 class Bonezegei_BMP180{
 public:
   Bonezegei_BMP180();
 
   char begin();
 };
+
+#endif
